cefdba1b6a15afce6bb27f0b9904ba3c3737fe97
Boggu Sravani <sravaniboggu417@gmail.com>
2025-11-30 02:19:25 +0530

    Configured frontend to work with backend

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,13 +2,11 @@
 import axiosClient from "./axiosClient";
 
 export const registerApi = (payload) =>
-    axiosClient.post("/api/auth/register", payload);
+    axiosClient.post("/auth/register", payload);
 
 export const loginApi = (payload) =>
-    axiosClient.post("/api/auth/login", payload);
+    axiosClient.post("/auth/login", payload);
 
-export const getCurrentUserApi = () =>
-    axiosClient.get("/api/auth/me");
+export const getCurrentUserApi = () => axiosClient.get("/auth/me");
 
-export const logoutApi = () =>
-    axiosClient.post("/api/auth/logout");
+export const logoutApi = () => axiosClient.post("/auth/logout");
